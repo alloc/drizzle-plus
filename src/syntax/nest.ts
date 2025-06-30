4b533befa3fb80f980ec63b64a9b925cabce4406
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-06-30 16:22:09 -0400

    fix: avoid using getSelectedFields for relational queries
    It’s redundant since we use buildRelationalQuery.

--- src/syntax/nest.ts
+++ src/syntax/nest.ts
@@ -1,11 +1,6 @@
 import { QueryPromise, sql } from 'drizzle-orm'
 import type { AnyQuery, QueryToSQL, SQLExpression } from '../types'
-import {
-  buildRelationalQuery,
-  getDecoder,
-  getSelectedFields,
-  getSQL,
-} from '../utils'
+import { buildRelationalQuery, getDecoder, getSQL } from '../utils'
 
 /**
  * Wrap a subquery with parentheses and decode the result.
@@ -13,24 +8,28 @@ import {
  * **Please note** that the subquery must have exactly one column.
  */
 export function nest<T extends AnyQuery>(subquery: T) {
-  const selectedFields = getSelectedFields(subquery)
-  const keys = Object.keys(selectedFields)
-  if (keys.length !== 1) {
-    throw new Error('Subquery must have exactly one column')
-  }
-
   if (subquery instanceof QueryPromise) {
     const builtQuery = buildRelationalQuery(subquery)
-    const { field } = builtQuery.selection[0]
 
-    return sql`(${builtQuery.sql})`.mapWith(result => {
-      return getDecoder(field as SQLExpression).mapFromDriverValue(result)
-    }) as QueryToSQL<T, { unwrap: true }>
+    if (builtQuery.selection.length === 1) {
+      const { field } = builtQuery.selection[0]
+
+      return sql`(${builtQuery.sql})`.mapWith(result => {
+        return getDecoder(field as SQLExpression).mapFromDriverValue(result)
+      }) as QueryToSQL<T, { unwrap: true }>
+    }
+  } else {
+    const { selectedFields } = subquery._
+    const keys = Object.keys(selectedFields)
+
+    if (keys.length === 1) {
+      const field = selectedFields[keys[0] as string]
+
+      return sql`(${getSQL(subquery)})`.mapWith(result => {
+        return getDecoder(field as SQLExpression).mapFromDriverValue(result)
+      }) as QueryToSQL<T, { unwrap: true }>
+    }
   }
 
-  const field = selectedFields[keys[0] as string]
-
-  return sql`(${getSQL(subquery)})`.mapWith(result => {
-    return getDecoder(field as SQLExpression).mapFromDriverValue(result)
-  }) as QueryToSQL<T, { unwrap: true }>
+  throw new Error('Subquery must have exactly one column')
 }
