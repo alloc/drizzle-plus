4e6bd35d746c11db61a4626d3d43805c8fb064a8
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-07-01 14:56:26 -0400

    chore: clean up toSQL docs and add `ToSQL<T>` type

--- src/syntax/toSQL.ts
+++ src/syntax/toSQL.ts
@@ -1,24 +1,18 @@
-import { isSQLWrapper, sql, SQL } from 'drizzle-orm'
-import { SQLExpression } from '../types'
+import { isSQLWrapper, sql, SQL, SQLWrapper } from 'drizzle-orm'
+
+export type ToSQL<T> = T extends SQLWrapper
+  ? T
+  : T extends number | boolean | null
+    ? SQL<T>
+    : SQL<string>
 
 /**
- * Accepts any kind of value and returns a `SQLWrapper` instance. Strings are
- * escaped, constants are wrapped with `sql.raw()`, and objects are returned as
- * is.
- *
- * This function is useful in cases where a constant value is permitted, but so
- * is a SQL expression. It avoids using a query parameter for
- * numbers/booleans/null, while still escaping strings to prevent SQL injection.
+ * Coerce a JavaScript value to a parameter binding, while `SQLWrapper`
+ * instances are passed through.
  *
  * @param value - The value to coerce.
  * @returns A `SQLWrapper` instance.
  */
-export function toSQL<T>(
-  value: T
-): T extends SQLExpression<unknown>
-  ? T
-  : T extends number | boolean | null
-    ? SQL<T>
-    : SQL<string> {
-  return (isSQLWrapper(value) ? value : sql`${value}`) as any
+export function toSQL<T>(value: T): ToSQL<T> {
+  return (isSQLWrapper(value) ? value : sql`${value}`) as ToSQL<T>
 }
