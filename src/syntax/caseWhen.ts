e3008347a7c97eb95feee51a4cc27728d9c294b6
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-07-22 00:55:54 -0400

    feat(caseWhen): support undefined `when` expression
    Since the official `or`/`and` operators may return undefined as an optimization, caseWhen should handle that.

--- src/syntax/caseWhen.ts
+++ src/syntax/caseWhen.ts
@@ -2,40 +2,45 @@ import { SQL, sql } from 'drizzle-orm'
 import type { SQLExpression, SQLValue } from '../types'
 
 export class SQLCaseWhen<T = never> {
-  cases: SQL<T>
-  constructor(init?: SQL<T> | SQLCaseWhen<T>) {
-    // Clone the initial cases to enable re-use.
-    this.cases = init
-      ? sql`${init instanceof SQLCaseWhen ? init.cases : init}`
-      : sql<T>`CASE`
+  cases: SQL<T>[]
+  constructor(init?: SQLCaseWhen<T>) {
+    this.cases = init ? [...init.cases] : []
   }
 
   /**
    * Add a case to the case expression.
    */
-  when<Then>(whenExpr: SQLExpression, thenExpr: SQLValue<Then>) {
-    this.cases.append(sql` WHEN ${whenExpr} THEN ${thenExpr}`)
+  when<Then>(whenExpr: SQLExpression | undefined, thenExpr: SQLValue<Then>) {
+    if (whenExpr) {
+      this.cases.push(sql` WHEN ${whenExpr} THEN ${thenExpr}`)
+    }
     return this as SQLCaseWhen<T | Then>
   }
 
   /**
    * Add the else clause to the case expression.
    */
-  else<Else>(elseExpr: SQLValue<Else>) {
-    return sql`${this.cases} ELSE ${elseExpr} END` as SQL<T | Else>
+  else<Else>(elseExpr: SQLValue<Else>): SQL<T | Else> {
+    if (this.cases.length) {
+      return sql`CASE ${sql.join(this.cases, '')} ELSE ${elseExpr} END`
+    }
+    return sql`${elseExpr}`
   }
 
   /**
    * Finish the case expression without an else clause, which will
    * return `null` if no case matches.
    */
-  elseNull() {
-    return sql`${this.cases} END` as SQL<T | null>
+  elseNull(): SQL<T | null> {
+    if (this.cases.length) {
+      return sql`CASE ${sql.join(this.cases, '')} END`
+    }
+    return sql`NULL`
   }
 }
 
 export function caseWhen<Then>(
-  whenExpr: SQLExpression,
+  whenExpr: SQLExpression | undefined,
   thenExpr: SQLValue<Then>
 ) {
   return new SQLCaseWhen().when(whenExpr, thenExpr)
