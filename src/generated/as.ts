936331b5fb768dc65400f7a652d9620756617d9c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-07-23 02:09:28 -0400

    feat: add `as` method to `PgSelectBuilder` class
    This allows you to define a SELECT query without a FROM clause, and use it as a subquery or CTE.
    
    This commit also fixes e1d9c0f2, ensuring that fully-qualified identifiers are used when referencing a subquery created from a RelationalQuery instance.

--- src/generated/as.ts
+++ src/generated/as.ts
@@ -1,3 +1,4 @@
+// mysql-insert: import { PreparedQueryHKTBase } from 'drizzle-orm/mysql-core'
 import { getTableColumns, is, sql, SQL, Table, WithSubquery } from 'drizzle-orm'
 import {
   PgColumn,
@@ -29,33 +30,15 @@ declare module 'drizzle-orm/pg-core/query-builders/query' {
   }
 }
 
-PgRelationalQuery.prototype.as = function (
-  alias: string
-): PgRelationalSubquery<any, any> {
-  const selection: Record<string, unknown> = {}
-
-  // Derive the selection from the combination of `columns`, `with`, and `extras` options.
-  const selectedFields = getSelectedFields(this)
-  for (const key in selectedFields) {
-    if (selectedFields[key] !== undefined && selectedFields[key] !== false) {
-      selection[key] = sql`${sql.identifier(key)}`.as(key)
-    }
-  }
-
-  // https://github.com/drizzle-team/drizzle-orm/blob/109ccd34b549030e10dd9cd27e41641d0878a856/drizzle-orm/src/pg-core/db.ts#L175
-  return new Proxy(
-    new WithSubquery(getSQL(this), selection, alias, true),
-    new SelectionProxyHandler({
-      alias,
-      sqlAliasedBehavior: 'alias',
-      sqlBehavior: 'error',
-    })
-  )
+PgRelationalQuery.prototype.as = function (alias: string) {
+  return createWithSubquery(getSQL(this), getSelectedFields(this), alias)
 }
 
 declare module 'drizzle-orm/pg-core' {
   interface PgSelectBuilder<
     TSelection extends SelectedFields | undefined,
+    // mysql-insert: TPreparedQueryHKT extends PreparedQueryHKTBase,
+    // sqlite-insert: TResultType extends 'sync' | 'async', TRunResult,
     TBuilderMode extends 'db' | 'qb',
   > {
     as<TAlias extends string>(
@@ -80,19 +63,10 @@ PgSelectBuilder.prototype.as = function (alias) {
   }
 
   const selection = dialect.buildSelection(orderSelectedFields(fields))
-
-  // https://github.com/drizzle-team/drizzle-orm/blob/109ccd34b549030e10dd9cd27e41641d0878a856/drizzle-orm/src/pg-core/db.ts#L175
-  return new Proxy(
-    new WithSubquery(sql`select ${selection}`, fields, alias, true),
-    new SelectionProxyHandler({
-      alias,
-      sqlAliasedBehavior: 'alias',
-      sqlBehavior: 'error',
-    })
-  )
+  return createWithSubquery(sql`select ${selection}`, fields, alias)
 }
 
-// https://github.com/drizzle-team/drizzle-orm/blob/109ccd34b549030e10dd9cd27e41641d0878a856/drizzle-orm/src/utils.ts#L74
+// Adapted from https://github.com/drizzle-team/drizzle-orm/blob/109ccd34b549030e10dd9cd27e41641d0878a856/drizzle-orm/src/utils.ts#L74
 function orderSelectedFields(
   fields: Record<string, unknown>,
   pathPrefix?: string[]
@@ -115,4 +89,29 @@ function orderSelectedFields(
     }
   }
   return result
+}
+
+function createWithSubquery(
+  query: SQL,
+  selectedFields: Record<string, unknown>,
+  alias: string
+): any {
+  const selection: Record<string, SQL> = {}
+  for (const key in selectedFields) {
+    if (Object.hasOwn(selectedFields, key) && selectedFields[key]) {
+      // The identifier must be fully-qualified to avoid ambiguity.
+      selection[key] = sql`${sql.identifier(alias)}.${sql.identifier(key)}`
+    }
+  }
+
+  // Adapted from https://github.com/drizzle-team/drizzle-orm/blob/109ccd34b549030e10dd9cd27e41641d0878a856/drizzle-orm/src/pg-core/db.ts#L175
+  return new Proxy(
+    new WithSubquery(query, selection, alias, true),
+    new SelectionProxyHandler({
+      alias,
+      // All values of `selection` are SQL objects. This option allows accessing
+      // the SQL object directly by its field name.
+      sqlBehavior: 'sql',
+    })
+  )
 }
