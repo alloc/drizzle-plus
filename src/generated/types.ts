12de95028b0442099c483787435ab90ac512689a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-04 23:37:36 -0400

    feat(types): add `RawFieldsToSubquery` type to `./[dialect]/types` entry point

--- src/generated/types.ts
+++ src/generated/types.ts
@@ -1,4 +1,7 @@
+import { ColumnBaseConfig, ColumnDataType, SQL, Subquery } from 'drizzle-orm'
+import { PgColumn } from 'drizzle-orm/pg-core'
 import type { RelationalQueryBuilder } from 'drizzle-orm/pg-core/query-builders/query'
+import { RawFieldsToSelection } from 'drizzle-plus/types'
 
 export type { RelationalQueryBuilder }
 
@@ -14,4 +17,30 @@ export type { RelationalQueryBuilder }
 export type InferColumns<T extends RelationalQueryBuilder<any, any>> =
   T extends RelationalQueryBuilder<any, infer TFields>
     ? TFields['columns']
+    : never
+
+type RawFieldsToColumnsSelection<T extends Record<string, unknown>> =
+  RawFieldsToSelection<T> extends infer TSelection
+    ? {
+        [K in keyof TSelection]: TSelection[K] extends infer TExpression
+          ?
+              | TExpression
+              | (TExpression extends SQL.Aliased<infer TData>
+                  ? PgColumn<
+                      ColumnBaseConfig<ColumnDataType, string> & {
+                        data: Exclude<TData, null>
+                        notNull: TData | null extends TData ? false : true
+                      }
+                    >
+                  : never)
+          : never
+      }
+    : never
+
+export type RawFieldsToSubquery<
+  T extends Record<string, unknown>,
+  TAlias extends string = string,
+> =
+  RawFieldsToColumnsSelection<T> extends infer TSelection
+    ? Subquery<TAlias, TSelection & Record<string, unknown>> & TSelection
     : never
